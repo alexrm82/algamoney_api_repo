574f7a2958eba058ce7e26c93328d3c9c26f4ed9
Pc <Pc@192.168.15.12>
2019-04-22 13:29:40 -0300

    commiting

--- algamoney-api/src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
@@ -1,14 +1,21 @@
 package com.example.algamoneyapi.resource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +23,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 
 import com.example.algamoneyapi.event.RecursoCriadoEvent;
+import com.example.algamoneyapi.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.example.algamoneyapi.model.Lancamento;
 import com.example.algamoneyapi.model.Lancamento;
 import com.example.algamoneyapi.repository.LancamentoRepository;
+import com.example.algamoneyapi.service.LancamentoService;
+import com.example.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -28,6 +39,13 @@ public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private LancamentoService lancamentoService;
+	
+	@Autowired
+	private MessageSource messageSource;
+	
 	
 	@GetMapping
 	public List<Lancamento> listar() {
@@ -47,14 +65,23 @@ public class LancamentoResource {
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 		
-		Lancamento lancamentoSalva = lancamentoRepository.save(lancamento);
-		
+		Lancamento lancamentoSalva = lancamentoService.salvar(lancamento);		
 		//evento que coloca no HeaderLocation das respostas http o codigo dos objetos salvos
-		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalva.getCodigo()));
-		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalva.getCodigo()));		
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalva);
 		
 		
+	}
+	
+	//excecao propria com base na logia de negocio no servico de lancamento que determina se pessoa pode
+	//receber lancamentos (nao pode ser nula nem inativa)
+	@ExceptionHandler({PessoaInexistenteOuInativaException.class})
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaExceptionn(PessoaInexistenteOuInativaException ex, WebRequest request) {
+		
+		String mensagemUsuario = messageSource.getMessage("pessoa.pessoa-inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();		
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario,mensagemDesenvolvedor));
+		return ResponseEntity.badRequest().body(erros);
 	}
 	
 }
